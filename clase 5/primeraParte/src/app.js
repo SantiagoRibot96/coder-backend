d35e7255aca98d2502361faae3c1dc6820149c86
SantiagoRibot96 <85650377+santiagoribot@users.noreply.github.com>
2024-02-17 14:13:55 -0300

    Clase 5

--- clase 5/primeraParte/src/app.js
+++ clase 5/primeraParte/src/app.js
@@ -14,3 +14,29 @@ app.use("/api/pets", petsRouter);
 app.listen(PUERTO, () => {
     console.log(`Escuchando en el puerto ${PUERTO}`);
 });
+
+// app.use(express.static("public"));
+
+app.use("/static", express.static("public"));
+
+/* import express from "express";
+import multer from "multer";
+
+const app = express();
+const PUERTO = 8080;
+app.use(express.json());
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "public/img");
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.originalname);
+    }
+});
+
+const upload = multer({storage: storage});
+
+app.post("/upload", upload.single("imagen"), (req, res) => {
+    res.send("Imagen creada");
+}); */
